67cd64dbb72ce1c22865ac6b051ee1c07ddaf51c
José Neto <josenetopr5@gmail.com>
2020-06-03 22:27:43 -0300

    Front-end done

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import Routes from './routes';
+
+// JSX: sintaxe de XML dentro do Javascript
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello World</h1>
-      </header>
-    </div>
+    <Routes />
   );
 }
 
